fe87065ffea33b363a478895a8f99bcb0ac81ed3
Saria Dorneles <sariadornels35@gmail.com>
2024-09-17 11:25:36 -0300

    autor
    autor

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textAUTOR1 = findViewById(R.id.textAUTOR1);
+        TextView textAUTOR2 = findViewById(R.id.textAUTOR2);
 
     }
 }
