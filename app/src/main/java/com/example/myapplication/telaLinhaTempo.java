c190a09c00f01a367f2b2ee4cce1cddff1b80f09
Halls <109175578+HallsRaul@users.noreply.github.com>
2024-09-18 11:05:46 -0300

    função dos botões pronta

--- app/src/main/java/com/example/myapplication/telaLinhaTempo.java
+++ app/src/main/java/com/example/myapplication/telaLinhaTempo.java
@@ -22,9 +22,110 @@ public class telaLinhaTempo extends AppCompatActivity {
                 showPopup1();
             }
         });
+        Button btnATO2 = findViewById(R.id.btnATO2);
+        btnATO2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopup2();
+            }
+        });
+        Button btnATO3 = findViewById(R.id.btnATO3);
+        btnATO3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopup3();
+            }
+        });
+        Button btnATO4 = findViewById(R.id.btnATO4);
+        btnATO4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopup4();
+            }
+        });
+        Button btnATO5 = findViewById(R.id.btnATO5);
+        btnATO5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopup5();
+            }
+        });
+        Button btnATO6 = findViewById(R.id.btnATO6);
+        btnATO6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopup6();
+            }
+        });
     }
 
     private void showPopup1() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Título")
+                .setMessage("Olá, o Felipe é doido!")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void showPopup2() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Título")
+                .setMessage("Olá, o Felipe é doido!")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    private void showPopup3() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Título")
+                .setMessage("Olá, o Felipe é doido!")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    private void showPopup4() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Título")
+                .setMessage("Olá, o Felipe é doido!")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    private void showPopup5() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Título")
+                .setMessage("Olá, o Felipe é doido!")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    private void showPopup6() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Título")
                 .setMessage("Olá, o Felipe é doido!")
