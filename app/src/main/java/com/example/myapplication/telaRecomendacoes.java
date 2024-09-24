1744cef101cc688604d92c644c2dcebbee046877
Saria Dorneles <sariadornels35@gmail.com>
2024-09-24 11:23:09 -0300

    PERFIL RECOMENDAÇÕES

--- app/src/main/java/com/example/myapplication/telaRecomendacoes.java
+++ app/src/main/java/com/example/myapplication/telaRecomendacoes.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class telaRecomendacoes extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class telaRecomendacoes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_recomendacoes);
+        TextView textRECO = findViewById(R.id.textRECO);
+
+        ImageView imgFILME = findViewById(R.id.imgFILME);
+        TextView textFILME2 = findViewById(R.id.textFILME2);
+        TextView textFILME3 = findViewById(R.id.textFILME3);
+
+        ImageView imgJOGO = findViewById(R.id.imgJOGO);
+        TextView textJOGO2 = findViewById(R.id.textJOGO);
+        TextView textJOGO3 = findViewById(R.id.textJOGO2);
+        ImageView imgARTIGO = findViewById(R.id.imgARTIGO);
+
+
+
     }
 }
