099187708a6988bc85b55e2fcbfdd85a29567bb7
Saria Dorneles <sariadornels35@gmail.com>
2024-10-08 08:07:15 -0300

    aoaiaiai

--- app/src/main/java/com/example/myapplication/telaResenha.java
+++ app/src/main/java/com/example/myapplication/telaResenha.java
@@ -1,6 +1,14 @@
 package com.example.myapplication;
 
-public class telaResenha {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 
+public class telaResenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_resenha);
+    }
 }
