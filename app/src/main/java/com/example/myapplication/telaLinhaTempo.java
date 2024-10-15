f332d89864eb9c1b8847b1d7359605d9579fcfa6
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2024-10-15 10:52:11 -0300

    btnVoltar LinhaTempo

--- app/src/main/java/com/example/myapplication/telaLinhaTempo.java
+++ app/src/main/java/com/example/myapplication/telaLinhaTempo.java
@@ -3,9 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AlertDialog;
 
 public class telaLinhaTempo extends AppCompatActivity {
@@ -14,6 +17,14 @@ public class telaLinhaTempo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_linha_tempo);
+        ImageButton voltar = findViewById(R.id.btnVoltar);
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent voltar = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(voltar);
+            }
+        });
 
         Button btnATO1 = findViewById(R.id.btnATO1);
         btnATO1.setOnClickListener(new View.OnClickListener() {
